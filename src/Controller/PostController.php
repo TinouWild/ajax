788c3110bc5f24948657fd1df38d3f592f7aa289
TinouWild <tinouclt@gmail.com>
2019-05-04 13:54:33 +0200

    save

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\PostLike;
+use App\Repository\PostLikeRepository;
 use App\Repository\PostRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
@@ -11,10 +16,53 @@ class PostController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(PostRepository $repo)
-    {
+    public function index(PostRepository $repo){
         return $this->render('post/index.html.twig', [
             'posts' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/post/{id}/like", name="post_like")
+     * @param Post $post
+     * @param ObjectManager $manager
+     * @param PostLikeRepository $likerepo
+     * @return Response
+     */
+    public function like(Post $post, ObjectManager $manager, PostLikeRepository $likerepo) : Response
+    {
+        $user = $this->getUser();
+        if (!$user) return $this->json([
+            'code' => 403,
+            'message' => 'Il faut être conecté'
+        ], 403);
+
+        if ($post->isLikedByUser($user)) {
+            $like = $likerepo->findOneBy([
+                'post' => $post,
+                'user' => $user
+            ]);
+
+            $manager->remove($like);
+            $manager->flush();
+
+            return $this->json([
+                'code' => 200,
+                'message' => 'Like bien supprimé',
+                'likes' => $likerepo->count(['post' => $post])
+            ],200);
+        }
+
+        $like = new PostLike();
+        $like->setPost($post)
+            ->setUser($user);
+        $manager->persist($like);
+        $manager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => 'Like bien ajouté',
+            'likes' => $likerepo->count(['post' => $post])
+        ], 200);
     }
 }
